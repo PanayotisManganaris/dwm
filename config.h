1428d09de14215cfcdfa0053e3b56fc00c655bcf
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-02-05 00:40:52 -0500

    bind nyxt browser

--- config.h
+++ config.h
@@ -91,6 +91,7 @@ static const char *termcmd[]     = { "st", NULL };
 static const char *dwmdiecmd[]   = { "pkill", "dwm", NULL };
 static const char *emacsclient[] = { "emacsclient", "-c", NULL };
 static const char *passmenucmd[] = { "passmenu", NULL };
+static const char *browsecmd[]   = { "nyxt", NULL };
 static const char *clipin[]      = { "clipcat-menu", "--finder=dmenu", "insert", NULL };
 static const char *clipdel[]     = { "clipcat-menu", "--finder=dmenu", "remove", NULL };
 static const char *upvol[]   = { "/home/panos/.local/bin/updatevolume.sh", "+", NULL };
@@ -113,6 +114,7 @@ static Key keys[] = {
         { MODKEY|ShiftMask,             XK_p,                     spawn,          {.v = passmenucmd } },
         { MODKEY|ShiftMask,             XK_Return,                spawn,          {.v = termcmd     } },
         { MODKEY,                       XK_e,                     spawn,          {.v = emacsclient } },
+        { MODKEY,                       XK_w,                     spawn,          {.v = browsecmd   } },
         { MODKEY,                       XK_y,                     spawn,          {.v = clipin      } },
         { MODKEY|ShiftMask,             XK_y,                     spawn,          {.v = clipdel     } },
         //facade
